0caf7c7afd833778b0adacb859b31e5eaf8ad9b1
Graham Bates <info@grahambates.com>
2022-05-27 14:17:02 +0100

    Export BreakpointStorage interface

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export * from "./disassembly/cpuDisassembler";
 export * from "./disassembly/copperDisassembler";
 export * from "./disassembly/disassembledFile";
 export * from "./customRegisters";
+export { BreakpointStorage } from "./breakpoints";
